80ccc12009c119b0c5d12616b7522698a37af3bc
Karthik Rao <raokarthik74@gmail.com>
2016-07-11 01:00:09 -0700

    Changes the first cell - makes it a part of table view, enabling scrolling

---
+++ thirty/FirstCell.h
@@ -0,0 +1,13 @@
+//
+//  FirstCell.h
+//  thirty
+//
+//  Created by Karthik Rao on 7/11/16.
+//  Copyright © 2016 Karthik Rao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstCell : UITableViewCell
+
+@end
---
+++ thirty/FirstCell.m
@@ -0,0 +1,24 @@
+//
+//  FirstCell.m
+//  thirty
+//
+//  Created by Karthik Rao on 7/11/16.
+//  Copyright © 2016 Karthik Rao. All rights reserved.
+//
+
+#import "FirstCell.h"
+
+@implementation FirstCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
